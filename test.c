6a998bc1be1af93c38410e1964f5af3f868962f6
klima7 <ukasz.klimkiewicz@gmail.com>
2021-03-28 14:06:46 +0200

    Test program improved

--- test.c
+++ test.c
@@ -5,64 +5,96 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <signal.h>
+#include <math.h>
 #include "siglog.h"
 
-char* dump_handler();
 
-char a = 1;
-short b = 1;
-int c = 1;
-long d = 1;
+// -------- Fibbonacci task -------
 
-float e = 1;
-double f = 1;
+int fib_a;
+int fib_b;
+int fib_c;
 
-unsigned char g = 1;
-unsigned short h = 1;
-unsigned int i = 1;
-unsigned long j = 1;
-
-void dump_function1(FILE *file) {
-    fprintf(file, "a: %c\n", a);
-    fprintf(file, "b: %d\n", b);
-    fprintf(file, "c: %d\n", c);
+void fibbonacci_dump(FILE *dump) {
+    fprintf(dump, "fib_a: %d\n", fib_a);
+    fprintf(dump, "fib_b: %d\n", fib_b);
+    fprintf(dump, "fib_c: %d\n", fib_c);
 }
 
-void dump_function2(FILE *file) {
-    fprintf(file, "e: %f\n", e);
-    fprintf(file, "f: %lf\n", f);
+void *fibbonacci_thread(void *arg) {
+    siglog_register_dump_function(fibbonacci_dump);
+
+    siglog_standard("Starting calculating fibonacci numbers");
+    fib_a = 1;
+    fib_b = 1;
+
+    while(1) {
+        fib_c = fib_a + fib_b;
+        siglog_standard("Next fibonacci number is %d", fib_c);
+
+        sleep(1);
+
+        fib_a = fib_b;
+        fib_b = fib_c;
+        siglog_min("Fibonacci variables swaped");
+
+        if(fib_c > 1000) {
+            siglog_max("Reseting fibonacci variables");
+            fib_a = 1;
+            fib_b = 1;
+        }
+
+        sleep(1);
+    }
 }
+
+// --------- prime task ----------
+
+int prime_next;
+
+void prime_dump(FILE *dump) {
+    fprintf(dump, "prime_next: %d\n", prime_next);
+}
+
+int is_prime(int num) {
+    for(int i=2; i<=sqrt(num); i++) {
+        if(num % i == 0)
+            return 1;
+    }
+    return 0;
+}
+
+void *prime_thread(void *arg) {
+    siglog_register_dump_function(prime_dump);
+    siglog_standard("Starting searching prime numbers");
+    prime_next = 2;
+    while(1) {
+        siglog_min("Checking whether number %d is prime", prime_next);
+        if(is_prime(prime_next)) {
+            siglog_max("Prime number %d found", prime_next);
+        }
+        prime_next++;
+        sleep(1);
+    }
+}
+
+// ------------- main --------------
 
 int main() {
+
+    pthread_t fibonacci_thread_tid, prime_thread_tid;
 
     // Signal 34
     printf("pid=%d\n", getpid());
 
-    int res = siglog_init(SIGRTMIN, SIGRTMIN+1, SIGLOG_DISABLED, NULL);
-    assert(res == 0);
+    siglog_init(-1, -1, SIGLOG_STANDARD, NULL);
 
-    siglog_register_dump_function(dump_function1);
-    siglog_register_dump_function(dump_function2);
+    // Create threads
+    pthread_create(&fibonacci_thread_tid, NULL, fibbonacci_thread, NULL);
+    pthread_create(&prime_thread_tid, NULL, prime_thread, NULL);
 
-    sleep(2);
+    // Join threads
+    while(1) sleep(1);
 
-    while(1) {
-        siglog_max("Hello there %d", 4);
-        siglog_standard("Hello there %d", 4);
-        siglog_min("Hello there %d", 4);
-        a++;
-        b++;
-        c++;
-        d++;
-        e++;
-        f++;
-        g++;
-        h++;
-        i++;
-        j++;
-        sleep(2);
-    }
-
-    siglog_free();
     return 0;
 }
