1f591d42dbd58652bb68c1d5ae7556ce1d1264b8
klima7 <ukasz.klimkiewicz@gmail.com>
2021-03-28 14:25:00 +0200

    Bug in is_prime function fixed

--- test.c
+++ test.c
@@ -61,9 +61,9 @@ void prime_dump(FILE *dump) {
 int is_prime(int num) {
     for(int i=2; i<=sqrt(num); i++) {
         if(num % i == 0)
-            return 1;
+            return 0;
     }
-    return 0;
+    return 1;
 }
 
 void *prime_thread(void *arg) {
